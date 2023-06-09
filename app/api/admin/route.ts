03d8b02842d39eebc2c5b19c20bc3c6701c992c3
Oddur Magnusson <oddur@klang-games.com>
2023-06-09 12:37:07 +0000

    fix: product deletion

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -6,7 +6,6 @@ import { Database } from '@/types_db';
 import { log } from 'next-axiom';
 import { v4 as uuidv4 } from 'uuid';
 
-
 export async function POST(req: Request) {
 
   // todo: add auth check
