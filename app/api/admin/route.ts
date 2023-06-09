0db8330181acdc8bc46a083c9d5df7208215e15e
Oddur Magnusson <oddur@klang-games.com>
2023-06-09 12:26:15 +0000

    fix: product deletion

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -4,31 +4,34 @@ import { stripe } from '@/utils/stripe';
 import { upsertProductRecord } from '@/utils/supabase-admin';
 import { Database } from '@/types_db';
 import { log } from 'next-axiom';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export async function POST(req: Request) {
 
   // todo: add auth check
+  const requestId = uuidv4();
 
   const body = await req.text();
   const action = JSON.parse(body).action;
   const supabase = createRouteHandlerClient<Database>({ cookies });
 
-  log.info('Admin API called.', { action: action });
+  log.info('Admin API called.', { action: action, requestId: requestId });
 
   switch (action) {
     case 'stripe-sync-products': {
       let products_updates = 0;
       let products_deletes = 0;
-      log.info('Admin API: Syncing Stripe Products');
+      log.info('Admin API: Syncing Stripe Products', { requestId: requestId });
       const listAllProducts = await stripe.products.list();
       if (listAllProducts.lastResponse.statusCode != 200) {
-        log.error(`Error fetching products from Stripe.`, { statusCode: listAllProducts.lastResponse.statusCode });
+        log.error(`Error fetching products from Stripe.`, { statusCode: listAllProducts.lastResponse.statusCode, requestId: requestId });
         return;
       }
 
       // iterate over the product
       for (const product of listAllProducts.data) {
-        log.info(`Upserting product ${product.id}`);
+        log.info(`Upserting product ${product.id}`, { requestId: requestId, product: product.id  });
         // product does not exist, create it
         await upsertProductRecord(product);
         products_updates++;
@@ -36,26 +39,31 @@ export async function POST(req: Request) {
 
       const dbProducts = await supabase.from('products').select('*');
       if (dbProducts.error != null) {
-        log.error(`Error fetching products from database`, {error: dbProducts.error});
+        log.error(`Error fetching products from database`, { error: dbProducts.error, requestId: requestId });
         return;
       }
 
       for (const dbProduct of dbProducts.data) {
         // check if the dbProduct exists in Stripe
         if (listAllProducts.data.find(p => p.id == dbProduct.id) == null) {
-          log.info(`Deleting product ${dbProduct.id}`);
+          log.info(`Deleting product ${dbProduct.id}`, { requestId: requestId, product: dbProduct.id});
           // product does not exist, delete it
           const deletion = await supabase.from('products').delete().eq('id', dbProduct.id);
           if (deletion.error != null) {
-            log.error(`Error deleting product.`, {productId: dbProduct.id, error: deletion.error})
+            log.error(`Error deleting product.`, {
+              productId: dbProduct.id,
+              error: deletion.error,
+              statusText: deletion.statusText,
+              requestId: requestId,
+            });
           }
           products_deletes++;
         }
       }
-      return new Response(JSON.stringify({ products_updates, products_deletes }), {status: 200});
+      return new Response(JSON.stringify({ products_updates, products_deletes, requestId }), { status: 200 });
     }
   }
 
-  return new Response(JSON.stringify({ "error": "unknown action" }), {status: 500});
+  return new Response(JSON.stringify({ 'error': 'unknown action', requestId }), { status: 500 });
 
 }
